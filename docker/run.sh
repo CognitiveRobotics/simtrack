a766f927401775b0e4bc84b37b82627e87bb4493
ruffsl <roxfoxpox@gmail.com>
2015-11-07 18:43:16 -0500

    Adding more mounts to .run not working though, think i have a driver mismatch with host using cuda.run

--- docker/run.sh
+++ docker/run.sh
@@ -5,7 +5,13 @@ docker run \
 	--env="DISPLAY" \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--volume="/lib/modules/:/lib/modules/:ro" \
+	--volume="/etc/modprobe.conf/:/etc/modprobe.conf/:ro" \
+	--volume="/usr/share/nvidia/:/usr/share/nvidia/:ro" \
 	--device /dev/nvidia0:/dev/nvidia0 \
+	--device /dev/nvidia1:/dev/nvidia1 \
 	--device /dev/nvidiactl:/dev/nvidiactl \
-	--device /dev/bus/usb:/dev/bus/usb:rwm \
+	--device /dev/nvidia-uvm:/dev/nvidia-uvm \
+	--device /dev/bus/usb:/dev/bus/usb \
+	--privileged \
 	$@
