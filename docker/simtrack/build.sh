2a4e5eb35785bcd6c975cb1f2a3f1ee7c699be1b
ruffsl <roxfoxpox@gmail.com>
2015-11-07 17:11:51 -0500

    Adding Dockerfile to build simtrack and pointing .rosinstall to cogrob repo

--- docker/simtrack/build.sh
+++ docker/simtrack/build.sh
@@ -1,15 +1,20 @@
+
 #!/bin/bash
 # set -e
 
-# source /opt/ros/indigo/setup.bash
-# source ~/catkin_ws/devel/setup.bash
-#
-# cd ~/catkin_ws/src
-#
-# catkin_init_workspace
-#
-# cd ..
-#
-# catkin_make || true
-# catkin_make || true
-# catkin_make
+source /opt/ros/indigo/setup.bash
+mkdir /root/ws/src -p
+cd /root/ws/src
+wget -O .rosinstall https://raw.githubusercontent.com/CognitiveRobotics/simtrack/master/simtrack.rosinstall
+wstool update
+
+# install dependencies
+cd ..
+rm /etc/ros/rosdep/sources.list.d/20-default.list
+rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src -y
+
+# build code
+source /opt/ros/indigo/setup.bash
+catkin_make -DCMAKE_BUILD_TYPE="Release"
